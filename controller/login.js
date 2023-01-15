28340af0dde9656655e2d6dc172109a75e4c1c94
hung1012 <57567619+hung1012@users.noreply.github.com>
2023-01-15 15:11:47 +0700

    feature: add login api

--- controller/login.js
+++ controller/login.js
@@ -1,12 +1,35 @@
 const { async } = require('@firebase/util');
 const {db} = require('../config/firebase');
+const jwt = require('jsonwebtoken')
 
 module.exports.login = async (req, res) => {
     const username = req.body.username
     const password = req.body.password
-    const all_match = []
-    const entry = await db.collection('account').where("username", "==", username).get()
-    console.log(entry)
-    entry.forEach( (doc) => all_match.push(doc.data()));
-    return res.status(200).send("User data: ",all_match)
+    db.collection('account')
+    .where("username", "==", username)
+    .where("password", "==", password)
+    .get()
+    .then((querySnapshot) => {
+        // querySnapshot.forEach((doc) => all_match.push(doc.data()))
+        querySnapshot.forEach((doc) => {
+            all_match = doc.data()
+        })
+        if (querySnapshot.size!=0) {
+            const token = jwt.sign(all_match.id, "SUPPER_SERRECT_KEY")
+            res.cookie('token', token, { httpOnly: true })
+            res.status(200).send({
+                message: "Login successfully !",
+                token: token,
+                data: all_match})
+            console.log("Login successfully")
+        }
+        else {
+            res.send("Account not exist, please register or login again !")
+            console.log("Login fail !")
+        }
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+        res.status(500).send(error)
+    })
 }
