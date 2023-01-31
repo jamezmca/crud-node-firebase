5b30dbc00e56ba13b7947914107362f61724ed67
Minh Nghia <dankkingmeo2@gmail.com>
2023-01-31 17:00:49 +0700

    add try catch

--- controller/getHoKhau.js
+++ controller/getHoKhau.js
@@ -3,16 +3,20 @@ const size = 1
 
 module.exports.hokhau_pagination = async (req, res, next) => {
     
-    const page = parseInt(req.query.page)
-    const first = await db.collection('hokhau').orderBy('IDchuho')
-                        .limit(size*page)
-                        .get()
-    const last = first.docs[first.docs.length -1];
-    const list_account = []
-    const querydb = await db.collection('hokhau').orderBy('IDchuho')
-                            .startAfter(last.data().IDchuho)
-                            .limit(size).get()
-    querydb.forEach((doc) => list_account.push(doc.data()))
-    return res.status(200).json(list_account)
+    try{
+        const page = parseInt(req.query.page)
+        const first = await db.collection('hokhau').orderBy('IDchuho')
+                            .limit(size*page)
+                            .get()
+        const last = first.docs[first.docs.length -1];
+        const list_account = []
+        const querydb = await db.collection('hokhau').orderBy('IDchuho')
+                                .startAfter(last.data().IDchuho)
+                                .limit(size).get()
+        querydb.forEach((doc) => list_account.push(doc.data()))
+        return res.status(200).json(list_account)
+    } catch(error){
+        return res.status(500).json(error.message);
+    }
     
 }
--- controller/getNhanKhau.js
+++ controller/getNhanKhau.js
@@ -2,16 +2,20 @@ const {db} = require('../config/firebase');
 const size = 1
 
 module.exports.nhankhau_pagination = async (req, res, next) => {
-    const page = parseInt(req.query.page)
-    const first = await db.collection('nhankhau').orderBy('id')
-                        .limit(size*page)
-                        .get()
-    const last = first.docs[first.docs.length -1];
-    const list_account = []
-    const querydb = await db.collection('nhankhau').orderBy('id')
-                            .startAfter(last.data().id)
-                            .limit(size).get()
-    querydb.forEach((doc) => list_account.push(doc.data()))
-    return res.status(200).json(list_account)
+    try{
+        const page = parseInt(req.query.page)
+        const first = await db.collection('nhankhau').orderBy('id')
+                            .limit(size*page)
+                            .get()
+        const last = first.docs[first.docs.length -1];
+        const list_account = []
+        const querydb = await db.collection('nhankhau').orderBy('id')
+                                .startAfter(last.data().id)
+                                .limit(size).get()
+        querydb.forEach((doc) => list_account.push(doc.data()))
+        return res.status(200).json(list_account)
+    } catch(error){
+        return res.status(500).json(error.message);
+    }
     
 }
