02137efdf2262287d2533b8deceedc5379af7a5b
hung1012 <57567619+hung1012@users.noreply.github.com>
2023-02-02 00:04:15 +0700

    add cursor based pagination

--- controller/getHoKhau.js
+++ controller/getHoKhau.js
@@ -2,21 +2,31 @@ const {db} = require('../config/firebase');
 const size = 1
 
 module.exports.hokhau_pagination = async (req, res, next) => {
-    
     try{
-        const page = parseInt(req.query.page)
-        const first = await db.collection('hokhau').orderBy('IDchuho')
-                            .limit(size*page)
-                            .get()
-        const last = first.docs[first.docs.length -1];
-        const list_account = []
-        const querydb = await db.collection('hokhau').orderBy('IDchuho')
-                                .startAfter(last.data().IDchuho)
-                                .limit(size).get()
-        querydb.forEach((doc) => list_account.push(doc.data()))
-        return res.status(200).json(list_account)
-    } catch(error){
+      let n_cursor = req.query.nextCursor
+      let size = parseInt(req.query.size)
+      const list_hokhau = []
+      if (n_cursor) {
+        console.log(n_cursor);
+        const querydb = await db.collection('hokhau').orderBy('id')
+                                .startAt(n_cursor)
+                                .limit(size+1)
+                                .get()
+        querydb.forEach((doc) => list_hokhau.push(doc.data()))
+      }
+      else {
+        const querydb = await db.collection('hokhau').orderBy('id')
+                                .limit(size+1)
+                                .get()
+        querydb.forEach((doc) => list_hokhau.push(doc.data()))
+      }
+      return res.status(200).json({
+        data: list_hokhau.slice(0,size),
+        nextCursor: list_hokhau[size].id,
+        size: size   
+      })
+      } 
+    catch(error){
         return res.status(500).json(error.message);
-    }
-    
-}
+      } 
+  }
--- controller/getNhanKhau.js
+++ controller/getNhanKhau.js
@@ -3,19 +3,29 @@ const size = 1
 
 module.exports.nhankhau_pagination = async (req, res, next) => {
     try{
-        const page = parseInt(req.query.page)
-        const first = await db.collection('nhankhau').orderBy('id')
-                            .limit(size*page)
-                            .get()
-        const last = first.docs[first.docs.length -1];
-        const list_account = []
+      let n_cursor = req.query.nextCursor
+      let size = parseInt(req.query.size)
+      const list_nhankhau = []
+      if (n_cursor) {
         const querydb = await db.collection('nhankhau').orderBy('id')
-                                .startAfter(last.data().id)
-                                .limit(size).get()
-        querydb.forEach((doc) => list_account.push(doc.data()))
-        return res.status(200).json(list_account)
-    } catch(error){
+                                .startAt(n_cursor)
+                                .limit(size+1)
+                                .get()
+        querydb.forEach((doc) => list_nhankhau.push(doc.data()))
+      }
+      else {
+        const querydb = await db.collection('nhankhau').orderBy('id')
+                                .limit(size+1)
+                                .get()
+        querydb.forEach((doc) => list_nhankhau.push(doc.data()))
+      }
+      return res.status(200).json({
+        data: list_nhankhau.slice(0,size),
+        nextCursor: list_nhankhau[size].id,
+        size: size   
+      })
+      } 
+    catch(error){
         return res.status(500).json(error.message);
-    }
-    
-}
+      } 
+  }
